523dc96e35e529964bc9b40e5c73a547584d2825
NguyenPhuong09082004 <bestleesin01001@gmail.com>
2025-09-20 20:50:52 +0700

    p

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,8 @@
 // controllers/authController.js
+
+// mảng users tạm (chỉ lưu khi server đang chạy, restart thì mất)
+let users = [];
+
 exports.loginForm = (req, res) => {
   res.render('login', { error: null });
 };
@@ -11,11 +15,12 @@ exports.forgotForm = (req, res) => {
   res.render('forgot', { error: null });
 };
 
+// ---- LOGIN ----
 exports.login = (req, res) => {
   const { email, password } = req.body;
 
-  // TODO: kiểm tra DB, tạm thời hardcode
-  if (email === 'admin@gmail.com' && password === '123') {
+  const user = users.find(u => u.email === email && u.password === password);
+  if (user) {
     req.session.user = { email };
     return res.redirect('/suppliers');
   }
@@ -23,19 +28,30 @@ exports.login = (req, res) => {
   res.render('login', { error: 'Sai tài khoản hoặc mật khẩu' });
 };
 
+// ---- REGISTER ----
 exports.register = (req, res) => {
   const { email, password } = req.body;
-  // TODO: lưu user vào DB
-  console.log('New user:', email, password);
+
+  // kiểm tra trùng email
+  const existingUser = users.find(u => u.email === email);
+  if (existingUser) {
+    return res.render('register', { error: 'Email đã tồn tại' });
+  }
+
+  // thêm user mới
+  users.push({ email, password });
+  console.log('Danh sách users:', users);
+
   res.redirect('/auth/login');
 };
 
+// ---- FORGOT ----
 exports.forgot = (req, res) => {
   const { email } = req.body;
-  // TODO: gửi mail reset password
   res.send(`Email reset đã được gửi tới: ${email}`);
 };
 
+// ---- LOGOUT ----
 exports.logout = (req, res) => {
   req.session.destroy();
   res.redirect('/auth/login');
